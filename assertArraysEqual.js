7afe27373d11b2c87bdb0f2cb3a79a691fa83f03
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-09-16 02:56:59 +0000

    assert arrays equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArray = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      return true;
+  } 
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArray(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertArraysEqual([1,2,3], [1,2,3]);
